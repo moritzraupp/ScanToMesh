739e8c5cc326416793b6417b2e868bfe5429760a
moritzraupp <moritz.raupp@stud-mail.uni-wuerzburg.de>
2025-07-22 01:07:30 +0200

    rescale image filter bugfix: pixel type was a string

--- PythonScripts/ImageProcessors/Rescale.py
+++ PythonScripts/ImageProcessors/Rescale.py
@@ -5,7 +5,7 @@ import itk
 import ImageIO as io
 
 class RescaleImageFilter(ImageProcessor):
-    def __init__(self, out_min=0, out_max=255, out_type=itk.UC):
+    def __init__(self, out_min=0, out_max=255, out_type="UC"):
         super().__init__()
         self.out_min = out_min
         self.out_max = out_max
@@ -18,8 +18,18 @@ class RescaleImageFilter(ImageProcessor):
         if 'rgb' in str(self.pixel_type).lower():
             raise Exception('RescaleImageFilter.process() only works with grayscale images!')
 
+        pixel_type_map = {
+            "UC": itk.UC,
+            "US": itk.US,
+        }
+
+        pt = pixel_type_map.get(self.out_type)
+        if pt is None:
+            allowed_types = ", ".join(pixel_type_map.keys())
+            raise Exception(f"Unsupported pixel type: {self.out_type}. Currently allowed types: {allowed_types}")
+
         InputImageType = itk.Image[self.pixel_type, self.dimension]
-        OutputImageType = itk.Image[self.out_type, self.dimension]
+        OutputImageType = itk.Image[pt, self.dimension]
 
 
         input_image = self.image
